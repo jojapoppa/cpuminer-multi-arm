d41d3e3a361a9711e4d6235c40d5ef076affed9e
Joja Poppa <jojapops@protonmail.com>
2020-11-29 15:38:31 +0000

    added mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,4 @@
-./cpuminer -a argon -o stratum+tcp://argon2d-dyn.na.mine.zergpool.com:4239 -u 4ziWABN18nTTWTLJ28Hj4bMR2DNBGHwJYS -p c=XMY,mc=XMY
+./cpuminer -a yescrypt -o stratum+tcp://yescrypt.na.mine.zergpool.com:6233 -u 4tdM7jpSx1EJHbYY8afUNuSZughpsSib7R -p c=XMY,mc=XMY
+
+#-a argon -o stratum+tcp://argon2d-dyn.na.mine.zergpool.com:4239 -u 4ziWABN18nTTWTLJ28Hj4bMR2DNBGHwJYS -p c=XMY,mc=XMY
+#-a yescrypt -o stratum+tcp://yescrypt.na.mine.zergpool.com:6233 -u 4ziWABN18nTTWTLJ28Hj4bMR2DNBGHwJYS -p c=XMY,mc=XMY
